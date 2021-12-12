8a6390cbc1658dd6755dc5f316d8a08938584671
LucasMSF <lucasgg003@gmail.com>
2021-12-12 01:42:51 -0300

    Primeiro Commit do projeto

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const URL = 'https://api.github.com/users/';
+var usuario = '';
+
+const txtUsuario = $('#txtUsuario');
+const btnProcurar = $('#btn');
+const divResultado = $('#Resultado')
+
+btnProcurar.click(function () { 
+    
+    usuario = txtUsuario.val();
+
+    $.ajax({
+        type: "GET",
+        url: `${URL}${usuario}`,
+        success: function (resposta) {
+            console.log(resposta)
+            renderizarPerfil(resposta)    
+        },
+        error: function (erro) {
+            console.log(erro);
+            let msgErro = erro.responseJSON.message
+            msgErro == 'Not Found' ? alert('Usuário não encontrado!') : alert('Erro Desconhecido!')
+        }
+    });
+    
+});
+
+function renderizarPerfil({login, bio, avatar_url, location, url}) {
+    divResultado.empty();  
+    divResultado.append(
+        $('<img></img>', {
+            src: avatar_url,
+            alt: 'Imagem de perfil'
+        }),
+        $(`<h3>${login}</h3>`),
+        $(`<h4>${bio}</h4>`),
+        $(`<h4>${location}</h4>`),
+        $(`<h4>${url}</h4>`) 
+    );
+}
+
+
