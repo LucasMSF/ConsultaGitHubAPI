bec83e172b412fd73e6a858a848162b062c38b34
Lucas Farias <lucas-farias@grupoamp.local>
2021-12-20 09:21:38 -0300

    Alterações de tratamento e frontEnd

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
+//constante para guardar a URL da requisição
 const URL = 'https://api.github.com/users/';
+//variável para guardar o usuario digitado pelo cliente
 var usuario = '';
 
+//Constantes que armazenam os obejetos da DOM
 const txtUsuario = $('#txtUsuario');
 const btnProcurar = $('#btn');
 const divResultado = $('#Resultado')
 
+//Programando o evento click do Botão
 btnProcurar.click(function () { 
     
+    //Puxando usuário digitado pelo cliente 
     usuario = txtUsuario.val();
 
+    //Declarando a requisição da biblioteca jquery
     $.ajax({
         type: "GET",
         url: `${URL}${usuario}`,
+        //callback de sucesso
         success: function (resposta) {
             console.log(resposta)
             renderizarPerfil(resposta)    
         },
+        //callback de erro
         error: function (erro) {
             console.log(erro);
             let msgErro = erro.responseJSON.message ? erro.responseJSON.message : erro.statusText;
@@ -25,6 +33,7 @@ btnProcurar.click(function () {
     
 });
 
+//Função resposável por adicionar os elementos HTML com os dados recebidos da requisição
 function renderizarPerfil({login, bio, avatar_url, location, html_url}) {
     divResultado.empty();
     divResultado.css({display: 'none'});
@@ -41,6 +50,7 @@ function renderizarPerfil({login, bio, avatar_url, location, html_url}) {
         $(`<h4><a href=${html_url} target="_blank">${html_url}</a></h4>`)
     );
 
+    //Animção de FadeIn 
     divResultado.fadeIn(400);
 }
 
